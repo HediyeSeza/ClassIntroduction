f909f8f8b40426dd17298acbd417117b75fd5bbd
sezahedi <sezahedi13@gmail.com>
2022-07-16 01:14:20 +0430

    Add project files.

---
+++ ClassIntroduction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIntroduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User hedi = new User();
+
+            Console.WriteLine("Please enter your name :");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Please enter your family :");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Please enter your BirthDay :");
+            string Year = Console.ReadLine();
+            string Month = Console.ReadLine();
+            string Day = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Your Fullname is : ");
+            Console.WriteLine(hedi.FullName(FirstName, LastName));
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Your BirthDay is : ");
+            Console.WriteLine(hedi.BirthDay(Year, Month, Day));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassIntroduction/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIntroduction
+{
+    internal class User
+    {
+        public string FullName(string FirstName, string LastName)
+        {
+            return FirstName + " " + LastName;
+        }
+        public string BirthDay(string Year, string Month, string Day)
+        {
+            return Year + "/" + Month + "/" + Day;
+        }
+    }
+}
